cead300a8f2fe4dd8f6aea7ddda461915e67f55f
Jeferson <jeferson@jeferson-laptop.(none)>
2014-02-05 21:19:46 -0200

    Skeleton para conexao com rethinkdb

--- server/persistence/db.js
+++ server/persistence/db.js
@@ -1,5 +1,7 @@
 var db = (function() {
 	'use strict';
+
+	var rethinkDB = require('rethinkdb');
 	
 	var connection;
 
@@ -8,6 +10,19 @@ var db = (function() {
 	}
 
 	function establishesConnection() {
+		rethinkDB.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+			if(err) throw err;
+
+			rethinkDB.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
+				if(err) throw err;
+				
+				console.log(res);
+				rethinkDB.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res) {
+				    if(err) throw err;
+				    console.log(res);
+				});
+			});
+		});
 	}
 
 	return {
