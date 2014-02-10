adf19eaf611a8bcb4a1dacb291c6d33707067f7a
Jeferson <jperito@wmixvideo.com.br>
2014-02-10 17:04:00 -0200

    Removido Rethinkdb driver

--- server/persistence/db.js
+++ server/persistence/db.js
@@ -1,29 +1,13 @@
 var db = (function() {
 	'use strict';
 
-	var rethinkDB = require('rethinkdb');
-	
 	var connection;
 
 	function init() {
 		establishesConnection();
 	}
 
-	function establishesConnection() {
-		rethinkDB.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
-			if(err) throw err;
-
-			rethinkDB.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
-				if(err) throw err;
-				
-				console.log(res);
-				rethinkDB.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res) {
-				    if(err) throw err;
-				    console.log(res);
-				});
-			});
-		});
-	}
+	function establishesConnection() {}
 
 	return {
 		init: init
