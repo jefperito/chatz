ddeeeedfc10e4e638ee46623c2147f048f6cae53
Jeferson Viana Perito <jefperito@gmail.com>
2014-03-30 10:25:03 -0300

    Cache será um arquivo a parte

--- server/repositories/users.js
+++ server/repositories/users.js
@@ -1,26 +1,6 @@
 var users = (function() {
     'use strict';
 
-    var redis = require('redis');
-    var client = redis.createClient();
-
-    client.get('chatz:users', function(error, result) {
-        if (error) {
-            console.error('Redis: Nao foi possivel obter os usuarios');
-            return;
-        }
-
-        if (result === null) {
-            console.log('Redis: Criando a lista de usuarios');
-
-            client.set('chatz:users', function(error, result) {
-                if (error) {
-                    console.log('Redis: Nao foi possivel fixar os usuarios');
-                }
-            });
-        }
-    });
-
     var usersMap = {};
 
     function getMap() {
